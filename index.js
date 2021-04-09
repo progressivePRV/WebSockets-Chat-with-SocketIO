6d96b070106d32214c49465d1741ea19902b557f
Prabhav Jani <31888813+progressivePRV@users.noreply.github.com>
2021-04-09 16:33:32 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const io = require('socket.io')(server);
+
+app.get('/', (req,res)=>{
+    // res.send('<h1> hello world</h1>');
+    res.sendFile(__dirname+'/index.html');
+});
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+    socket.on('chat message', (msg) => {
+        // console.log('message: ' + msg);
+        io.emit('chat message', msg);
+    });
+    socket.on('disconnect', ()=>{
+        console.log('user disconnected');
+    });
+});
+
+server.listen(3000, ()=>{
+    console.log('listening on http://localhost:3000');
+});
